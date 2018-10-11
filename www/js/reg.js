cf84d3daf2e2fd312e94fe2fa00ae119f6715a3b
dzh0n <plotnikov.evgeniy@gmail.com>
2018-10-11 16:11:03 +0500

    registration

--- www/js/reg.js
+++ www/js/reg.js
@@ -1,7 +1,39 @@
+
+
 $(document).ready(function(){
     $('input.phone-mask').mask('+7 (000) 000-00-00', {clearIfNotMatch: true});
+
+    $('#sms_code').val(randomInteger(1000,9999));
+
+    $('.ajax_sms').on('click',function(){
+        sendSms();
+        return false
+    });
+
 });
 
 function registration() {
-    alert();
+
+}
+
+function sendSms() {
+    if($('#phone').val() != '') {
+        $.ajax({
+            type: "POST",
+            url: "https://мужичок-грузовичок.рф/mapi/registration/sms",
+            data: "phone="+$('#phone').val()+'&code='+$('#sms_code').val(),
+            success: function(msg){
+                alert( "Прибыли данные: " + msg );
+            }
+        });
+    }
+    else {
+        alert('Укажите сначала свой номер телефона');
+    }
+}
+
+function randomInteger(min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    rand = Math.round(rand);
+    return rand;
 }
