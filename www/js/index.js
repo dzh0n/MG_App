d6f7d0076b5d918bb864ac241e4c95928a7d5669
dzh0n <plotnikov.evgeniy@gmail.com>
2018-10-14 00:58:25 +0500

    login

--- www/js/index.js
+++ www/js/index.js
@@ -74,3 +74,19 @@ $(document).ready(function () {
     });*/
 });
 
+
+function openNav() {
+    //document.getElementById("mySidenav").style.width = "250px";
+    $('#mySidenav').css('width','250px');
+    // document.getElementById("main").style.marginLeft = "250px";
+    //document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    $('body').css('backgroundColor','rgba(0,0,0,0.4)');
+}
+
+function closeNav() {
+    $('#mySidenav').css('width','0');
+    //document.getElementById("mySidenav").style.width = "0";
+    //document.getElementById("main").style.marginLeft= "0";
+    //document.body.style.backgroundColor = "white";
+    $('body').css('backgroundColor','rgba(255,255,255,1)');
+}
