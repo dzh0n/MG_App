aecc2a7b7f1a351409d19fede11f39f06fb7c2a7
dzh0n <plotnikov.evgeniy@gmail.com>
2018-10-15 01:11:30 +0500

    push

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -66,9 +66,9 @@ document.addEventListener("deviceready", function(){
         cordova.plugins.notification.local.on("click", function (notification) {
             var inData = JSON.parse(notification.data);
             alert(inData.typePush);
-            if(parseInt(inData.typePush) < 2) {
+            /*if(parseInt(inData.typePush) < 2) {
                 window.location = 'order.html#'+notification.id;
-            }
+            }*/
         });
 
     }
@@ -133,10 +133,10 @@ function getPush() {
                         id: result.order_id,
                         title: title,
                         text: result.message,
-                        data: { typePush: result.type_push },
+                        data: { typePush: "_"+result.type_push },
                         vibrate: true,
                         lockscreen: true,
-                        led: "FF0000",
+                        led: "#FF0000",
                     });
                 }
             }
