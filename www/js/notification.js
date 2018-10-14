16af28f816a5c263a14ab29657e5918cacd09f7c
dzh0n <plotnikov.evgeniy@gmail.com>
2018-10-14 16:24:07 +0500

    setting notify

--- www/js/notification.js
+++ www/js/notification.js
@@ -35,11 +35,24 @@ $(document).ready(function(){
 });
 
 function saveSettings() {
+    is_active = 0;
+    if($('#is_active').is(':checked'))
+        is_active = 1;
+    type_g = 0;
+    if($('#type_g').is(':checked'))
+        type_g = 1;
+    type_s = 0;
+    if($('#type_s').is(':checked'))
+        type_s = 1;
+    type_sm = 0;
+    if($('#type_sm').is(':checked'))
+        type_sm = 1;
+
     $.ajax({
         type: "POST",
         beforeSend: function(){/*SpinnerDialog.show();*/},
         url: "https://xn----dtbckhdelflyecx2bh6dk.xn--p1ai/mapi/notifications/savesettings",
-        data: "id="+userId+"&is_active="+$('#is_active').val()+"&region_id="+$('#region_id :selected').val()+'&type_g='+$('#type_g').val()+"&type_s="+$('#type_s').val()+"&type_sm="+$('#type_sm').val(),
+        data: "id="+userId+"&is_active="+is_active+"&region_id="+$('#region_id :selected').val()+'&type_g='+type_g+"&type_s="+type_s+"&type_sm="+type_sm,
         success: function(msg){
             if(msg=='success') {
                 // SpinnerDialog.hide();
